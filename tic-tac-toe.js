6947f35b1b08db9ca9a0a9c0657e8f16c478474d
Brianna Beharrie <brianna.beharrie@mymona.uwi.edu>
2023-10-22 21:36:18 -0500

    Modified JS

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -14,70 +14,92 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // Exercise 2
 document.addEventListener('DOMContentLoaded', function () {
-  squares.forEach((square, index) => {
+  const squares = document.querySelectorAll('#board > div');
+
+  squares.forEach((square) => {
     square.addEventListener('click', function () {
       if (!square.textContent) {
         square.textContent = currentPlayer;
         square.classList.add(currentPlayer);
         currentPlayer = players[(players.indexOf(currentPlayer) + 1) % players.length];
-      }
+      };
     });
   });
 });
 
 // Exercise 3
 document.addEventListener('DOMContentLoaded', function () {
-  squares.forEach((square) => {
-    square.addEventListener('mouseenter', function () {
-      square.classList.add('hover');
-    });
-    square.addEventListener('mouseleave', function () {
-      square.classList.remove('hover');
+    const squares = document.querySelectorAll('#board > div');
+  
+    squares.forEach((square) => {
+      square.addEventListener('mouseenter', function () {
+        square.classList.add('hover');
+      });
+      square.addEventListener('mouseleave', function () {
+        square.classList.remove('hover');
+      });
     });
   });
-});
 
 // Exercise 4
 function checkWinner() {
-  const winPatterns = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (const pattern of winPatterns) {
-    const [a, b, c] = pattern;
-    if (
-      squares[a].textContent &&
-      squares[a].textContent === squares[b].textContent &&
-      squares[a].textContent === squares[c].textContent
-    ) {
-      document.getElementById('status').textContent = `Congratulations! ${squares[a].textContent} is the Winner!`;
-      document.getElementById('status').classList.add('you-won');
+    const winPatterns = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+  
+    for (const pattern of winPatterns) {
+      const [a, b, c] = pattern;
+      const squares = document.querySelectorAll('#board > div');
+      if (
+        squares[a].textContent &&
+        squares[a].textContent === squares[b].textContent &&
+        squares[a].textContent === squares[c].textContent
+      ) {
+        document.getElementById('status').textContent = `Congratulations! ${squares[a].textContent} is the Winner!`;
+        document.getElementById('status').classList.add('you-won');
+      }
     }
   }
-}
+  
+  document.addEventListener('DOMContentLoaded', function () {
+    const squares = document.querySelectorAll('#board > div');
+  
+    squares.forEach((square) => {
+      square.addEventListener('click', function () {
+        if (!square.textContent) {
+          square.textContent = currentPlayer;
+          square.classList.add(currentPlayer);
+          checkWinner();
+          currentPlayer = players[(players.indexOf(currentPlayer) + 1) % players.length];
+        };
+      });
+    });
+  });
+  
 
 // Exercise 5
 document.addEventListener('DOMContentLoaded', function () {
-  const newGameButton = document.querySelector('.btn');
-
-  newGameButton.addEventListener('click', function () {
-    squares.forEach((square) => {
-      square.textContent = '';
-      square.className = 'square';
+    const newGameButton = document.querySelector('.btn');
+    const squares = document.querySelectorAll('#board > div');
+  
+    newGameButton.addEventListener('click', function () {
+      squares.forEach((square) => {
+        square.textContent = '';
+        square.className = 'square';
+      });
+  
+      document.getElementById('status').textContent = 'Move your mouse over a square and click to play an X or an O.';
+      document.getElementById('status').classList.remove('you-won');
+      currentPlayer = 'X';
     });
-
-    document.getElementById('status').textContent = 'Move your mouse over a square and click to play an X or an O.';
-    document.getElementById('status').classList.remove('you-won');
-    currentPlayer = 'X';
   });
-});
-
 // Exercise 6
 document.addEventListener('DOMContentLoaded', function () {
   squares.forEach((square) => {
