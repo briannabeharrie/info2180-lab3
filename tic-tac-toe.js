0503e34199a9fdc7d26323e04c8e1650b2016db5
Brianna Beharrie <brianna.beharrie@mymona.uwi.edu>
2023-10-22 20:24:18 -0500

    Edited JS file

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -33,3 +33,72 @@ document.addEventListener('DOMContentLoaded',function(){
           });
     });
 });
+//Exercise 4
+function checkWinner() {
+    const winPatterns = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (const pattern of winPatterns) {
+      const [a, b, c] = pattern;
+      if (
+        squares[a].textContent &&
+        squares[a].textContent === squares[b].textContent &&
+        squares[a].textContent === squares[c].textContent
+      ) {
+        document.getElementById('status').textContent = `Congratulations! ${squares[a].textContent} is the Winner!`;
+        document.getElementById('status').classList.add('you-won');
+      }
+    }
+  }
+  document.addEventListener('DOMContentLoaded', function () {
+    const squares = document.querySelectorAll('.square');
+  
+    squares.forEach((square, index) => {
+      square.addEventListener('click', function () {
+        if (!square.textContent) {
+          square.textContent = currentPlayer;
+          square.classList.add(currentPlayer);
+          checkWinner();
+          currentPlayer = players[(players.indexOf(currentPlayer) + 1) % players.length];
+        }
+      });
+    });
+  });
+//Exercise 5
+document.addEventListener('DOMContentLoaded', function () {
+    const newGameButton = document.querySelector('.btn');
+  
+    newGameButton.addEventListener('click', function () {
+      squares.forEach((square) => {
+        square.textContent = '';
+        square.className = 'square';
+      });
+  
+      document.getElementById('status').textContent = 'Move your mouse over a square and click to play an X or an O.';
+      document.getElementById('status').classList.remove('you-won');
+      currentPlayer = 'X';
+    });
+  });
+//Exercise 6
+document.addEventListener('DOMContentLoaded', function () {
+    const squares = document.querySelectorAll('.square');
+  
+    squares.forEach((square) => {
+      square.addEventListener('click', function () {
+        if (square.textContent === '') {
+          square.textContent = currentPlayer;
+          square.classList.add(currentPlayer);
+          checkWinner();
+          currentPlayer = players[(players.indexOf(currentPlayer) + 1) % players.length];
+        }
+      });
+    });
+  });
+  
