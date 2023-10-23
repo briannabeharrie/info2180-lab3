c7fcc35e63fd42883177cec4cd0e4bd1cf7573f0
Brianna Beharrie <brianna.beharrie@mymona.uwi.edu>
2023-10-22 19:58:48 -0500

    Edited JS file

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,25 @@
+//Exercise 1
 document.addEventListener('DOMContentLoaded', function () {
     const squares = document.querySelectorAll('.square');
-  
+    
     squares.forEach((square, index) => {
       square.classList.add('square');
     });
   });
-  
+//Exercise 2
+let currentPlayer ='X';
+const players =['X','O'];
+document.addEventListener('DOMContentLoaded',function(){
+    const squares =document.querySelectorAll('.square');
+    squares.forEach((square,index) => {
+        square.addEventListener('click',function(){
+            if (!square.textContent){
+                square.textContent = currentPlayer;
+                square.classList.add(currentPlayer);
+                currentPlayer = players[(players.indexOf(currentPlayer) + 1) % players.length];
+            }
+        })
+    })
+})
+//Exercise 3
+
